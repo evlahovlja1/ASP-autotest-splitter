9b2e74388e813fcb54534cf4595f1c71f68e05cd
Elvir Vlahovljak <73286824+evlahovlja1@users.noreply.github.com>
2021-10-26 14:35:10 +0200

    Add files via upload

---
+++ rastava.py
@@ -0,0 +1,25 @@
+import pprint
+import re
+
+full = ""
+
+with open('at.txt', 'r') as file:
+    next(file)
+    next(file)
+    next(file)
+    full = file.read()
+
+l = re.compile("T[0-9]+\n").split(full)
+
+i = 1
+
+for t in l:
+    oboje = re.compile("R:\n").split(t)
+    test = oboje[0]
+    rez = oboje[1]
+    with open(str(i) + '.txt', 'w') as file:
+        file.write(test)
+    with open(str(i) + '_r.txt', 'w') as file:
+        file.write(rez)
+
+    i = i+1
